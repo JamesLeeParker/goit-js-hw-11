a3a3a10b433298713565d6ce109429411113694a
Ivan Kostenko <89205729+JamesLeeParker@users.noreply.github.com>
2021-11-24 17:27:15 +0200

    ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import './css/styles.css';
+import '../css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
